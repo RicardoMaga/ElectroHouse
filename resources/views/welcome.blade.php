7e6417f11fc0719177902011d197fec8cf333f62
Ricardo <ricardontmagalhaes04@fmail.com>
2022-12-09 12:12:07 +0000

    home nav

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,36 +1,13 @@
-<div>
- 
-<nav>
-  <button><a href="/produtos">Produtos</a></button>
-  <button>Contacto</button>
-  <button class="hotbar_button"><a href="/login">Login</a></button>
-</nav>
 
-<style>
-  nav {
-    display: flex;
-    position: absolute;
-    top: 0;
-    width: 100%;
-    height: 50px;
-    
-  }
-  button {
-    padding: 10px 20px;
-    font-size: 23px;
-    background-color: #333;
-    color: #fff;
-    border: none;
-    cursor: pointer;
-  }
-  button:hover {
-    background-color: #444;
-  }
-  .hotbar_button{
-    
-   top: 0;
-   right: 0;
-  }
+<x-app-layout>
+<div class="bg-neutral-700 fixed top-0 w-full h-19 ">
   
-</style>
+  <button class="pt-4 pr-2 pl-2 pb-4 text-lg bg-neutral-700 text-white rounded-none hover:bg-gray-800 "><a href="/produtos">Produtos</a></button>
+  <button class="pt-4 pr-2 pl-2 pb-4 first-letter:text-lg bg-neutral-700 text-white rounded-none hover:bg-gray-800 ">Contacto</button>
+  
+  <button class=" pt-4 pr-3 pl-3 pb-4 text-lg bg-neutral-700 text-white rounded-none absolute right-0 top-0 hover:bg-gray-800 " ><a href="/login">Login</a></button>
+  
 </div>
+
+
+</x-app-layout>
